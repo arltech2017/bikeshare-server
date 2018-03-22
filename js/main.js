57faaf2621aa63aa6cb7b3bcc1549556a0a6743b
Edzon <edzon.quinteros@gmail.com>
2018-03-22 18:30:41 -0400

    Included alert functionaly with JS to display text

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 var alerting = document.getElementById('myalert');
-var span = document.getElementByClassName("close")[0];
+var span = document.getElementsByClassName("close")[0];
 
-function zonny_alert() {
+function zonny_alert(text) {
    alerting.style.display = "block";
+   //we need to change content of the alert block. 
+   // the p element which is in the .alert-content class
+   // the content should have a value .alert-content p
+   var text_element = document.querySelector(".alert-content p")
+   //you need to change the <p> element content to have as an ouput the variable text
+     text_element.innerText = text;
 }
 span.onclick = function() {
    alerting.style.display = "none";
