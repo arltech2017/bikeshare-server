32538c6c5f37e99acbff1da8170ba76f87f125e1
Edzon <edzon.quinteros@gmail.com>
2018-03-22 17:02:27 -0400

    Included Js/main.js to alert functionality

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
-var alert = document.getElementById('myalert');
-var btn-checkout = document.getElementById("checkout-bike");
+var alerting = document.getElementById('myalert');
+var span = document.getElementByClassName("close")[0];
+
+function zonny_alert() {
+   alerting.style.display = "block";
+}
+span.onclick = function() {
+   alerting.style.display = "none";
+}
+window.onclick = function(event) {
+   if (event.target == alerting) {
+     alerting.style.display = "none";
+   }
+}
