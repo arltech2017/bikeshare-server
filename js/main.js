1cb480594d6f068e6202750508c296a73a4efdec
Marco Sirabella <marco@sirabella.org>
2018-06-12 09:12:04 -0400

    Add individual bike button click functionality

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
-var alerting = document.getElementById('myalert');
-var span = document.getElementsByClassName("close")[0];
 
-function zonny_alert(text) {
+function zonny_alert(element_id, text) {
+   var alerting = document.getElementById(element_id);
+   var span = alerting.getElementsByClassName("close")[0];
+   window.onclick = function(event) {
+      if (event.target == alerting) {
+        alerting.style.display = "none";
+      }
+   }
+   span.onclick = function() {
+      alerting.style.display = "none";
+   }
    alerting.style.display = "block";
    //we need to change content of the alert block. 
    // the p element which is in the .alert-content class
    // the content should have a value .alert-content p
-   var text_element = document.querySelector(".alert-content p")
+   var text_element = alerting.querySelector(".alert-content p")
+   if (text_element) {
    //you need to change the <p> element content to have as an ouput the variable text
      text_element.innerText = text;
-}
-span.onclick = function() {
-   alerting.style.display = "none";
-}
-window.onclick = function(event) {
-   if (event.target == alerting) {
-     alerting.style.display = "none";
    }
 }
